f19e133e7ba652398e4f6e9c5b04f2e7748821d1
Mauro006 <maurodevsm23@gmail.com>
2026-05-29 13:31:39 -0300

    Refactorización de ListaProyectos.jsx

--- src/components/ListaProyectos.jsx
+++ src/components/ListaProyectos.jsx
@@ -1,35 +1,57 @@
 import { useState } from 'react';
 import proyectoService from '../services/proyectoService';
 import ProyectoCard from './ProyectoCard';
+import FormularioProyecto from './FormularioProyecto';
 import '../css/ListaProyectos.css';
 
 function ListaProyectos() {
 
-  const [proyectos, setProyectos] = useState(() => proyectoService.obtenerProyectos());
-  const [busqueda, setBusqueda] = useState(''); 
-  
-  const [nuevoTitulo, setNuevoTitulo] = useState('');
-  const [nuevaCategoria, setNuevaCategoria] = useState('');
-  const [nuevoEstado, setNuevoEstado] = useState('En curso');
-  const [nuevaDescripcion, setNuevaDescripcion] = useState('');
+  const [proyectos, setProyectos] = useState(() =>
+    proyectoService.obtenerProyectos()
+  );
+
+  const [busqueda, setBusqueda] = useState('');
+
+  const [formulario, setFormulario] = useState({
+    titulo: '',
+    categoria: '',
+    estado: 'En curso',
+    descripcion: ''
+  });
+
+  const { titulo, categoria, estado, descripcion } = formulario;
+
+  const manejarCambio = (e) => {
+    const { name, value } = e.target;
+
+    setFormulario({
+      ...formulario,
+      [name]: value
+    });
+  };
 
   const agregarProyecto = () => {
-    if (nuevoTitulo.trim() === '' || nuevaCategoria.trim() === '') return;
+
+    if (titulo.trim() === '' || categoria.trim() === '') return;
 
     const nuevoProyecto = {
       id: proyectos.length + 1,
-      titulo: nuevoTitulo,
-      categoria: nuevaCategoria,
-      estado: nuevoEstado,
-      descripcion: [nuevaDescripcion, ""] 
+      titulo,
+      categoria,
+      estado,
+      descripcion: [descripcion, ""]
     };
 
     proyectoService.agregarProyecto(nuevoProyecto);
+
     setProyectos(proyectoService.obtenerProyectos());
 
-    setNuevoTitulo('');
-    setNuevaCategoria('');
-    setNuevaDescripcion('');
+    setFormulario({
+      titulo: '',
+      categoria: '',
+      estado: 'En curso',
+      descripcion: ''
+    });
   };
 
   const eliminarProyecto = (id) => {
@@ -37,17 +59,18 @@ function ListaProyectos() {
     setProyectos(proyectoService.obtenerProyectos());
   };
 
-  const proyectosFiltrados = proyectos.filter(p =>
+  const proyectosFiltrados = proyectos.filter((p) =>
     p.titulo.toLowerCase().includes(busqueda.toLowerCase())
   );
 
   return (
     <section>
+
       <div>
-        <input 
-          className="btn-Buscar" 
-          type="text" 
-          placeholder="Buscar Proyectos" 
+        <input
+          className="btn-Buscar"
+          type="text"
+          placeholder="Buscar Proyectos"
           value={busqueda}
           onChange={(e) => setBusqueda(e.target.value)}
         />
@@ -63,9 +86,10 @@ function ListaProyectos() {
             <th colSpan="2">ACCIONES</th>
           </tr>
         </thead>
+
         <tbody>
-             {proyectosFiltrados.map((proyecto) => (
-             <ProyectoCard
+          {proyectosFiltrados.map((proyecto) => (
+            <ProyectoCard
               key={proyecto.id}
               proyecto={proyecto}
               eliminarProyecto={eliminarProyecto}
@@ -73,41 +97,12 @@ function ListaProyectos() {
           ))}
         </tbody>
       </table>
+     <FormularioProyecto 
+      form={formulario}
+      manejarCambio={manejarCambio} 
+      agregarProyecto={agregarProyecto} 
+    />
 
-      <div className="seccion-formulario">
-        <input 
-          type="text" 
-          className="caja-texto" 
-          placeholder="Título del proyecto" 
-          value={nuevoTitulo}
-          onChange={(e) => setNuevoTitulo(e.target.value)}
-        />
-        <input 
-          type="text" 
-          className="caja-texto" 
-          placeholder="Categoría" 
-          value={nuevaCategoria}
-          onChange={(e) => setNuevaCategoria(e.target.value)}
-        />
-        <select 
-          className="caja-texto" 
-          value={nuevoEstado}
-          onChange={(e) => setNuevoEstado(e.target.value)}
-        >
-          <option value="En curso">En curso</option>
-          <option value="Finalizado">Finalizado</option>
-          <option value="Pendiente">Pendiente</option>
-        </select>
-
-        <textarea 
-          className="caja-texto descripcion-input" 
-          placeholder="Escribe la descripción del proyecto..." 
-          value={nuevaDescripcion}
-          onChange={(e) => setNuevaDescripcion(e.target.value)}
-        />
-
-        <button className="btn-Agregar" onClick={agregarProyecto}>Agregar Proyecto</button>
-      </div>
     </section>
   );
 }
