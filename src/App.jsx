a4b74efdaad3cf218a4421a1067444ce2909ef76
Mauro006 <maurodevsm23@gmail.com>
2026-06-11 21:48:02 -0300

    feat: configurar React Router en App y main

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import Header from './components/Header';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
-import './css/styles.css';
+import Dashboard from './components/Dashboard';
 import ListaProyectos from './components/ListaProyectos';
-import { useEffect, useState } from 'react'
-
+import Perfil from './components/Perfil';
+import DetalleProyectoPage from './components/DetalleProyectoPage';
+import './css/styles.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
       <Nav />
-      
       <main style={{ minHeight: '50vh', padding: '20px', textAlign: 'center' }}>
-        <ListaProyectos />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/proyectos" element={<ListaProyectos />} />
+          <Route path="/proyectos/:id" element={<DetalleProyectoPage />} />
+          <Route path="/perfil" element={<Perfil />} />
+        </Routes>
       </main>
-      
       <Footer />
     </>
   )
